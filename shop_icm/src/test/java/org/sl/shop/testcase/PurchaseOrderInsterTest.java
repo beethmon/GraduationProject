10354675c27949ef92bfc8eb464dbeb5e738f4e1
Howard Rowe <beethmon@gmail.com>
2015-03-07 23:05:40 +0800

    我丢你老鼠浪费我成个钟头

--- shop_icm/src/test/java/org/sl/shop/testcase/PurchaseOrderInsterTest.java
+++ shop_icm/src/test/java/org/sl/shop/testcase/PurchaseOrderInsterTest.java
@@ -1,7 +1,9 @@
 package org.sl.shop.testcase;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,8 +31,8 @@ public class PurchaseOrderInsterTest {
 	//进货表自增id
 	private int poid;
 	//进货商店id
-	private Supplier spid;
-	//随机字符串
+	private int spid[] = {4,5,6,7,8};
+	//bno字符串
 	private String bno;
 	//进货项目商品单价
 	private List<PurchaseOrderItem> items;
@@ -68,13 +70,68 @@ public class PurchaseOrderInsterTest {
 	private int cState;
 	//商品随机码
 	private String ccodes;
-	
+	/**
+	 * 随机字串
+	 */
+
+	/**我丢你咩怒删
+	final String word[] = {"a","b","c","d","e","f",
+	                       "g","h","i","j","k","l",
+	                       "m","n","o","p","q","r",
+	                       "s","t","u","v","w","x",
+	                       "y","z",};
+    final String m[] = {"itg","ngt","sng","sgt",
+	                    "ggt","rkg","stg","htg",
+	                    "kgt","jug","jui","jun"};
+    final String w[] = {"nyb","gyb","kyb","syb","myb","kyb","dyb"};
+	static int i = 0;
+	static int j = 0;
+	 *
+	 * 取得字符串
+	 *
+//	@Test
+	public void getBNo(){
+		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
+		int month = calendar.get(Calendar.MONTH);
+		int week = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+		int day = calendar.get(Calendar.DAY_OF_MONTH) - 1;
+		String temp = "";
+		String temp2 = "";
+		if(day < 10){
+			temp = word[(int) (Math.random() * 26)] + word[(int) (Math.random() * 26)] + word[day];
+		}else{
+			temp = word[(int) (Math.random() * 26)] + word[(int) (day / 10)] + word[day % 10];
+		}		
+		int second = calendar.get(Calendar.SECOND);
+		if(i >= 26){
+			i = 0;
+		}
+		if(j >= 10){
+			j = 0;
+		}
+		if(second < 10){
+			temp2 = word[i] + "0" + j + second;
+		}else{
+			temp2 = word[i] + j + second;
+		}
+		String tempBno = "";
+		tempBno = m[month] + temp + w[week] + temp2;
+		System.out.println("bno为" + tempBno);
+		//最后记得++
+		i++;
+		j++;
+
+	}
+	**/
 	//获得所有商品
 	@Autowired
 	CommodityService commodityService;
 //	Commodity commodity = new Commodity();
 //	List<Commodity> listCommodity = getCommodity(commodity);
-	@Test
+//	@Test
+	/**
+	 * 取得商品列表
+	 */
 	public void getCommodity() {
 		// TODO Auto-generated method stub
 		Commodity commodity = new Commodity();
